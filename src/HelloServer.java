db276e9f125172d72765ba8d45eca501935aee97
Marcos <marcos.luz.noble@gmail.com>
2023-05-13 21:22:50 -0300

    Exemplo quebrado

---
+++ src/HelloServer.java
@@ -0,0 +1,31 @@
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+/* 
+Classname: HelloServerDemo 
+Purpose: The RMI server. 
+*/
+public class HelloServerDemo extends UnicastRemoteObject
+        implements Hello {
+    public HelloServerDemo() throws RemoteException {
+        super();
+    }
+
+    public String Hello() {
+        System.out.println("Invocation to Hello was succesful!");
+        return "Hello World from RMI server!";
+    }
+
+    public static void main(String args[]) {
+        try {
+// Creates an object of the HelloServer class. 
+            HelloServer obj = new HelloServer();
+// Bind this object instance to the name "HelloServer". 
+            Naming.rebind("Hello", obj);
+            System.out.println("Ligado no registro");
+        } catch (Exception ex) {
+            System.out.println("error: " + ex.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
